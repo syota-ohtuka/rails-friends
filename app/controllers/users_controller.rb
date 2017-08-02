a9ddb8de1637510ce4c303bc5810154781aafec1
ohtukasyota <syota.jaska@gmail.com>
2017-08-03 06:29:51 +0900

    編集追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :login_check, only: %i(index new edit create update destroy)
+	before_action :permission_check, only: %i(edit)
 	def new
 		@user = User.new
 	end
@@ -21,8 +23,38 @@ class UsersController < ApplicationController
 	def index
 	end
 
+	def show
+		@show_user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+    if @user.update(user_params)
+        redirect_to @user
+    else
+        render 'edit'
+    end
+	end
+
 	private
-		def user_params
-			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
+
+	def login_check
+    if session[:user_id].nil?
+    	redirect_to login_path
+    end
+	end
+
+	def permission_check
+		p current_user
+		if current_user.id == params[:id]
+			redirect_to user_path(@show_user_path)
 		end
+	end
 end

eb855bed95636a56532b8cfe9392113fca4e4222
ohtukasyota <syota.jaska@gmail.com>
2017-08-02 22:37:53 +0900

    開発しやすいようにuser検証緩くした

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
 	has_secure_password
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :name, presence: true
-	validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true, length: { minimum: 8 }
+
+	# 開発終わったらformat: { with: VALID_EMAIL_REGEX }追加
+	validates :email, presence: true, uniqueness: true
+
+	# 開発終わったらlength: { minimum: 8 }追加
+  validates :password, presence: true
 end
